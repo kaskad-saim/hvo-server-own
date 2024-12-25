a83a12f04cfe02b64e66f51d73626c22f43ca8ac
Zombiemas <maks.smerdov25@gmail.com>
2024-12-25 23:50:41 +0500

    small changes

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -9,11 +9,15 @@ const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 // Обновленные маршруты для страниц
-router.get('/mnemo-kotel-1', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/production/carbon/pechiVr', 'mnemo-pech-vr-1.html'));
+router.get('/mnemo-kotel1', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/production/utvh/kotelnaya/', 'mnemo-kotel1.html'));
 });
 
-router.get('/current-kotel-1', (req, res) => {
+router.get('/mnemo-kotel2', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../public/production/utvh/kotelnaya/', 'mnemo-kotel2.html'));
+});
+
+router.get('/current-kotel1', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/production/carbon/pechiVr', 'current-pech-vr-1.html'));
 });
 
