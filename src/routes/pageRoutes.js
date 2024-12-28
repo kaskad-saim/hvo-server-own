7f9f05fee6101ed8df2621cc516d35692ad5e950
kaskad-saim <saim.asutp@gmail.com>
2024-12-28 14:06:36 +0300

    comment old pages

--- src/routes/pageRoutes.js
+++ src/routes/pageRoutes.js
@@ -9,17 +9,17 @@ const __dirname = path.dirname(__filename);
 const router = express.Router();
 
 // Обновленные маршруты для страниц
-router.get('/mnemo-kotel1', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/production/utvh/kotelnaya/', 'mnemo-kotel1.html'));
-});
+// router.get('/mnemo-kotel1', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/production/utvh/kotelnaya/', 'mnemo-kotel1.html'));
+// });
 
-router.get('/mnemo-kotel2', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/production/utvh/kotelnaya/', 'mnemo-kotel2.html'));
-});
+// router.get('/mnemo-kotel2', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/production/utvh/kotelnaya/', 'mnemo-kotel2.html'));
+// });
 
-router.get('/current-kotel1', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/production/carbon/pechiVr', 'current-pech-vr-1.html'));
-});
+// router.get('/current-kotel1', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/production/carbon/pechiVr', 'current-pech-vr-1.html'));
+// });
 
 
 export default router;
