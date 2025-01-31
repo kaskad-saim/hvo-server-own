83997d27540b60857817c796f6ed44ff09988bc6
MaksSmerdov <C:\Users\MaximSmerdov\AppData\Roaming\The Bat!>
2025-01-31 09:23:18 +0500

    add routes graph hvo

--- src/routes/graphicRoutes.js
+++ src/routes/graphicRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import logger from '../logger.js';
 import { Kotel1Model, Kotel2Model, Kotel3Model } from '../models/kotelModel.js';
+import { Hvo1Model, Hvo2Model } from '../models/hvoModel.js';
 
 const router = express.Router();
 
@@ -40,6 +41,30 @@ router.get('/kotel3/data', async (req, res) => {
     res.json(data);
   } catch (error) {
     logger.error('Ошибка при получении данных Kotel3:', error);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
+});
+
+router.get('/hvo1/data', async (req, res) => {
+  try {
+    const { start, end } = req.query;
+    const query = start && end ? { lastUpdated: { $gte: new Date(start), $lte: new Date(end) } } : {};
+    const data = await Hvo1Model.find(query).sort({ lastUpdated: 1 });
+    res.json(data);
+  } catch (error) {
+    logger.error('Ошибка при получении данных hvo1:', error);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
+});
+
+router.get('/hvo2/data', async (req, res) => {
+  try {
+    const { start, end } = req.query;
+    const query = start && end ? { lastUpdated: { $gte: new Date(start), $lte: new Date(end) } } : {};
+    const data = await Hvo2Model.find(query).sort({ lastUpdated: 1 });
+    res.json(data);
+  } catch (error) {
+    logger.error('Ошибка при получении данных hvo2:', error);
     res.status(500).json({ error: 'Ошибка сервера' });
   }
 });
