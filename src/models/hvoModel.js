8dfbbefc0d51b538724171c9aba9f3694b27babc
Zombiemas <maks.smerdov25@gmail.com>
2025-02-08 01:06:04 +0500

    add index

--- src/models/hvoModel.js
+++ src/models/hvoModel.js
@@ -8,4 +8,6 @@ const hvoDataSchema = new mongoose.Schema({
 const Hvo1Model = mongoose.model('hvo1Models', hvoDataSchema);
 const Hvo2Model = mongoose.model('hvo2Models', hvoDataSchema);
 
+hvoDataSchema.index({ lastUpdated: 1 });
+
 export { Hvo1Model, Hvo2Model };
--- src/models/kotelModel.js
+++ src/models/kotelModel.js
@@ -13,5 +13,7 @@ const Kotel1Model = mongoose.model('kotel1Models', dataSchema);
 const Kotel2Model = mongoose.model('kotel2Models', dataSchema);
 const Kotel3Model = mongoose.model('kotel3Models', dataSchema);
 
+dataSchema.index({ lastUpdated: 1 });
+
 
 export { Kotel1Model, Kotel2Model, Kotel3Model };
